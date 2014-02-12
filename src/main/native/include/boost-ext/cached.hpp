3a4bca6701197211ec11c834f9207c7089a214d6
Nathan Toone <nathan@toonetown.com>
2014-02-12 14:28:50 -0700

    Change the syntax of this macro

--- src/main/native/include/boost-ext/cached.hpp
+++ src/main/native/include/boost-ext/cached.hpp
@@ -5,18 +5,25 @@
 #define H_BOOST_EXT_CACHED
 
 /* Calls a function and caches it in a static value (which is returned, if it is already set) */
-#define CACHED_FX(t, fx)                                        \
-    static t fx ## _load();                                     \
-    const t& fx() {                                             \
-        static t v;                                             \
-        static bool l;                                          \
-        if (!l) { v = fx ## _load(), l = true; }                \
-        return v;                                               \
-    }                                                           \
-    static t fx ## _load()
+#define CACHED_FX(t, fx)            CACHED_FX_IMPL(,t,fx)
+#define CACHED_STATIC_FX(t, fx)     CACHED_FX_IMPL(static, t, fx)
 
 /* Macro which can be used in a header file to define a cached function */
-#define CACHED_FX_DEF(t, fx) const t& fx()
-            
+#define CACHED_FX_DEF(t, fx)        const t& fx()
+
+/* Use this macro if you want to reference the load function directly */
+#define CACHED_LOAD_FX_NAME(fx)     fx ## _load
+
+/* Used internally for forward defining the load function */
+#define CACHED_LOAD_FX_DEF(t, fx)   static t CACHED_LOAD_FX_NAME(fx)()
+/* Macro with will work with either the static or non-static version */
+#define CACHED_FX_IMPL(s, t, fx)                                \
+    CACHED_LOAD_FX_DEF(t, fx);                                  \
+    s CACHED_FX_DEF(t, fx) {                                    \
+        static t v; static bool l;                              \
+        if (!l) { v = CACHED_LOAD_FX_NAME(fx)(), l = true; }    \
+        return v;                                               \
+    }                                                           \
+    CACHED_LOAD_FX_DEF(t, fx)
             
 #endif /* H_BOOST_EXT_CACHED */
